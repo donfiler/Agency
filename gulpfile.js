df047278428e3dfc200ad9c81dbd6e2856f4c501
Don Filer <dfiler@rocketmail.com>
2017-09-17 11:05:38 -0700

    added css and js minify gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require('gulp');
+
+gulp.task('default', function() {
+  // place code for your default task here
+var gulp = require('gulp');
 var mcss = require('gulp-mcss');
 var src = './src';
 var desc = './build';
@@ -9,4 +13,17 @@ gulp.task('mcss', ['kmc'], function() {
           src: '-debug.css'
         }))
         .pipe(gulp.dest(dest));
+});
+
+
+})
+var gulp = require('gulp');
+var jsmin = require('gulp-jsmin');
+var rename = require('gulp-rename');
+ 
+gulp.task('default', function () {
+    gulp.src('src/**/*.js')
+        .pipe(jsmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
 });
