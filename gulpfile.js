92bb39930e4deb12406d5370f1e1503e1af37ed5
Don Filer <dfiler@rocketmail.com>
2019-08-01 09:23:15 -0700

    Major chanes 5K

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,62 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
 
-gulp.task('default', function() {
-  // place code for your default task here
-var gulp = require('gulp');
-var mcss = require('gulp-mcss');
-var src = './src';
-var desc = './build';
- 
-gulp.task('mcss', ['kmc'], function() {
-   gulp.src(src+'/**/*.css')
-        .pipe(mcss({
-          src: '-debug.css'
-        }))
-        .pipe(gulp.dest(dest));
+/*
+  -- TOP LEVEL FUNCTIONS --
+  gulp.task - Define tasks
+  gulp.src - Point to files to use
+  gulp.dest - Points to folder to output
+  gulp.watch - Watch files and folders for changes
+*/
+
+// Logs Message
+gulp.task('message', function(){
+  return console.log('Gulp is running...');
 });
 
+// Copy All HTML files
+gulp.task('copyHtml', function(){
+  gulp.src('src/*.html')
+      .pipe(gulp.dest('dist'));
+});
 
-})
-var gulp = require('gulp');
-var jsmin = require('gulp-jsmin');
-var rename = require('gulp-rename');
- 
-gulp.task('default', function () {
-    gulp.src('src/**/*.js')
-        .pipe(jsmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist'));
+// Optimize Images
+gulp.task('imageMin', () =>
+	gulp.src('src/img/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/images'))
+);
+
+// Minify JS
+gulp.task('minify', function(){
+  gulp.src('src/js/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/js'));
+});
+
+// Compile Sass
+gulp.task('sass', function(){
+  gulp.src('src/sass/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('dist/css'));
+});
+
+// Scripts
+gulp.task('scripts', function(){
+  gulp.src('src/js/*.js')
+      .pipe(concat('main.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['message', 'copyHtml', 'imageMin', 'sass', 'scripts']);
+
+gulp.task('watch', function(){
+  gulp.watch('src/js/*.js', ['scripts']);
+  gulp.watch('src/img/*', ['imageMin']);
+  gulp.watch('src/sass/*.scss', ['sass']);
+  gulp.watch('src/*.html', ['copyHtml']);
 });
